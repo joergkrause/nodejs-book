271302582861ff38ab00ff4b90987c3f2bcd5abf
Joerg Krause <joerg@krause.net>
2017-10-04 16:39:26 +0200

    Refresh

---
+++ nodejs-app/server.js
@@ -0,0 +1,10 @@
+﻿var server = require("./start");
+var router = require("./router");
+var requestHandlers = require("./handlers");
+
+var handler = {};
+handler[["/", 'GET']] = requestHandlers.home;
+handler[["/show", 'GET']] = requestHandlers.show;
+handler[["/upload", 'POST']] = requestHandlers.upload;
+
+server.start(router.route, handler);
---
+++ nodejs-app/start.js
@@ -0,0 +1,43 @@
+﻿var http = require("http");
+var url = require("url");
+var formidable = require("formidable");
+var querystring = require("querystring");
+
+function start(route, handler) {
+  
+  function execute(pathname, handler, request, response, data) {
+    var content = route(pathname, handler, request, response, data);
+    if (!content) {
+      response.writeHead(400, { "Content-Type": "text/plain" });
+      response.write("400 Bad request");
+      response.end();
+    }
+  }
+  
+  function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    var query = url.parse(request.url).query;
+    if (request.method === 'POST') {
+      var form = new formidable.IncomingForm();
+      form.parse(request, function (err, fields, files) {
+        if (err) {
+          console.error(err.message);
+          return;
+        }
+        var data = { fields: fields, files: files };
+        execute(pathname, handler, request, response, data);
+      });
+    }
+    if (request.method === 'GET') {
+      var data = {
+        fields: querystring.parse(query)
+      };
+      execute(pathname, handler, request, response, data);
+    }
+  }
+  var port = process.env.port || 1337;
+  http.createServer(onRequest).listen(port);
+  console.log("Server gestartet.");
+}
+
+exports.start = start;
